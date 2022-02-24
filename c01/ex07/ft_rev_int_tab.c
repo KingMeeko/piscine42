183b156e89ac6fb1ece74e898932a37936e52146
KingMeeko <92309055+KingMeeko@users.noreply.github.com>
2022-02-23 23:00:55 -0500

    Update ft_rev_int_tab.c

--- c01/ex07/ft_rev_int_tab.c
+++ c01/ex07/ft_rev_int_tab.c
@@ -1,2 +1,20 @@
 #include <unistd.h>
 
+void    ft_rev_int_tab(int *tab, int size)
+{
+    int debut;
+    int fin;
+    int rev[size];
+    int temp;
+
+    debut = 0;
+    fin = size - 1;
+    while(debut < fin)
+        {
+        temp = tab[debut];
+        tab[debut] = tab[fin];
+        tab[fin] = temp;
+        debut++;
+        fin--;
+    }
+}
