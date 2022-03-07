edca2b06b2fed3877e3a6505e2f7125be4a24ee7
KingMeeko <92309055+KingMeeko@users.noreply.github.com>
2022-03-07 11:16:52 -0500

    Add files via upload

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: dantremb <dantremb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/27 10:27:51 by marvin            #+#    #+#             */
-/*   Updated: 2022/03/03 15:05:21 by dantremb         ###   ########.fr       */
+/*   Updated: 2022/03/07 11:15:09 by dantremb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,16 @@ char	*ft_strcapitalize(char *str)
 	i++;
 	while (str[i])
 	{
-		if (!((str[i - 1] >= 'A' && str[i - 1] <= 'Z') ||
-				(str[i - 1] >= 'a' && str[i - 1] <= 'z') ||
-				(str[i - 1] >= '0' && str[i - 1] <= '9')))
+		if (!((str[i - 1] >= 'A' && str[i - 1] <= 'Z') 
+				|| (str[i - 1] >= 'a' && str[i - 1] <= 'z')
+				|| (str[i - 1] >= '0' && str[i - 1] <= '9')))
 		{
-		if (str[i] >= 'a' && str[i] <= 'z')
+			if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] = str[i] - 32;
 		}
 		else
 		{
-		if (str[i] >= 'A' && str[i] <= 'Z')
+			if (str[i] >= 'A' && str[i] <= 'Z')
 			str[i] = str[i] + 32;
 		}
 	i++;
@@ -45,7 +45,6 @@ int	main(void)
 
 	printf("\nString a: %s\n", a);
 	printf("String b: %s\n", b);
-
 	printf("\nResult a: %s\n", ft_strcapitalize(a));
 	printf("\nResult b: %s\n", ft_strcapitalize(b));
 }
--- c02/ex10/ft_strlcpy.c
+++ c02/ex10/ft_strlcpy.c
@@ -6,26 +6,30 @@
 /*   By: dantremb <dantremb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/27 10:27:51 by marvin            #+#    #+#             */
-/*   Updated: 2022/03/03 15:07:26 by dantremb         ###   ########.fr       */
+/*   Updated: 2022/03/07 11:11:27 by dantremb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 #include <string.h>
-unsigned int ft_strlcpy(char *dest, char *src, unsigned int size)
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-	int i;
+	int	i;
+
 	i = 0;
-	if( size > 0 )
+	if (size > 0)
 	{
-		while(src[i] != '\0' && i < size)
+		while (src[i] != '\0' && i < size)
 		{
 			dest[i] = src[i];
 			i++;
 		}
 	}
 	dest[i] = '\0';
-	while(src[i])
+	while (src[i])
 		i++;
-	return(i);
+	return (i);
 }
 
 int		main(void)
@@ -37,5 +41,4 @@ int		main(void)
 	printf("dest : %s\n", dest);
 	ft_strlcpy(dest, src, size);
 	printf("dest : %s\n", dest);
-
 }
