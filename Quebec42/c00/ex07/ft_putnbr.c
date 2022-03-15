1c0e849046c58e1ac4c114032b0a6933eeeeb950
KingMeeko <92309055+KingMeeko@users.noreply.github.com>
2022-03-14 23:52:22 -0400

    Update ft_putnbr.c

--- Quebec42/c00/ex07/ft_putnbr.c
+++ Quebec42/c00/ex07/ft_putnbr.c
@@ -9,29 +9,31 @@
 /*   Updated: 2022/03/02 15:27:36 by dantremb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <unistd.h>
-
-void	ft_putnbr(int nb)
-	{
-	if (nb < 0)
-	{
-		ft_putchar('-');
-		nb = nb * -1;
-	}
-	if (nb > 9)
-	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
-	}
-	else
-	{
-		ft_putchar(nb + 48);
-	}
-}
-int main ()
 
 void	ft_putchar(char a)
 {
 	write(1, &a, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar('8');
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else
+	{
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+	}
+	ft_putchar(48 + nb % 10);
+	}
 }
